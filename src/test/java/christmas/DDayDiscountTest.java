619fac38831dad9dde69ec1a635486e94851456a
hyojun <hjlim45678@gmail.com>
2024-09-24 15:28:57 +0900

    feat: domain 뼈대 초안 구현

--- src/test/java/christmas/DDayDiscountTest.java
+++ src/test/java/christmas/DDayDiscountTest.java
@@ -1,7 +1,9 @@
 package christmas;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static jdk.internal.org.jline.reader.impl.LineReaderImpl.CompletionType.List;
 
+import christmas.discount.DDayDiscount;
+import christmas.discount.WeekDiscount;
 import java.util.stream.Stream;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -14,7 +16,7 @@ class DDayDiscountTest {
     @DisplayName("D-day이벤트 할인 금액 계산함수 성공 테스트")
     @ParameterizedTest
     @MethodSource("dDayDiscountProvider")
-    void calculateDiscountAmount(DecemberDate reserveDate, Money expected) {
+    void dDayDiscountAmount(DecemberDate reserveDate, Money expected) {
     //given
         DDayDiscount discount = new DDayDiscount();
     //when
@@ -38,6 +40,26 @@ class DDayDiscountTest {
                 Arguments.of(new DecemberDate(29), new Money(0)),
                 Arguments.of(new DecemberDate(30), new Money(0)),
                 Arguments.of(new DecemberDate(31), new Money(0))
+        );
+    }
+
+    @DisplayName("주말이벤트 할인 금액 계산함수 성공 테스트")
+    @ParameterizedTest
+    @MethodSource("weekDiscountProvider")
+    void weekDiscountAmount(DecemberDate reserveDate, Money expected) {
+        //given
+        WeekDiscount discount = new WeekDiscount();
+        //when
+        Money actual = discount.calculateDiscountAmount(reserveDate);
+        //then
+        Assertions.assertThat(actual.getAmount()).isEqualTo(expected.getAmount());
+    }
+
+    static Stream<Arguments> weekDiscountProvider() {
+        return  Stream.of(
+                Arguments.of(Menus, new Money(2023)),
+                Arguments.of(new Menus(List.of(Menu.CHOCO_CAKE, )), new Money(2023))
+
         );
     }
 }
