5f5581663e322feb03e1897b83e2d1315f430ed7
hyojun <hjlim45678@gmail.com>
2024-09-25 10:37:59 +0900

    feat : 특별 할인 기능 구현

--- src/test/java/christmas/DiscountTest.java
+++ src/test/java/christmas/DiscountTest.java
@@ -4,6 +4,7 @@ package christmas;
 import static org.assertj.core.api.Assertions.*;
 
 import christmas.discount.DDayDiscount;
+import christmas.discount.SpecialDiscount;
 import christmas.discount.WeekDiscount;
 import java.util.HashMap;
 import java.util.List;
@@ -19,6 +20,14 @@ class DiscountTest {
     //Fixture 도임
     private static final DecemberDate WEEKEND = new DecemberDate(1);
     private static final DecemberDate WEEKDAY = new DecemberDate(   3);
+
+    private static Menus makeMenusByList(List<Menu> menus) {
+        Map<Menu, Integer> menuRepository = new HashMap<>();
+        menus.forEach(menu -> {
+            menuRepository.put(menu, menuRepository.getOrDefault(menu, 0) + 1);
+        });
+        return new Menus(menuRepository);
+    }
     @DisplayName("D-day이벤트 할인 금액 계산함수 성공 테스트")
     @ParameterizedTest
     @MethodSource("dDayDiscountProvider")
@@ -92,11 +101,31 @@ class DiscountTest {
                 );
     }
 
-    private static Menus makeMenusByList(List<Menu> menus) {
-        Map<Menu, Integer> menuRepository = new HashMap<>();
-        menus.forEach(menu -> {
-            menuRepository.put(menu, menuRepository.getOrDefault(menu, 0) + 1);
-        });
-        return new Menus(menuRepository);
+    @DisplayName("달력에 별이 있는 날짜에는 1000원을 할인해준다.")
+    @ParameterizedTest
+    @MethodSource("specialDiscountProvider")
+    void specialDiscountTest(DecemberDate date, Money expected){
+        //given
+        SpecialDiscount specialDiscount = new SpecialDiscount();
+        //when
+        Money actual = specialDiscount.calculateDiscountAmount(date);
+        //then
+        assertThat(actual).isEqualTo(expected);
     }
+
+    static Stream<Arguments> specialDiscountProvider() {
+        return Stream.of(
+                Arguments.of(new DecemberDate(3), new Money(1000)),
+                Arguments.of(new DecemberDate(10), new Money(1000)),
+                Arguments.of(new DecemberDate(17), new Money(1000)),
+                Arguments.of(new DecemberDate(24), new Money(1000)),
+                Arguments.of(new DecemberDate(25), new Money(1000)),
+                Arguments.of(new DecemberDate(31), new Money(1000)),
+                Arguments.of(new DecemberDate(1), new Money(0)),
+                Arguments.of(new DecemberDate(2), new Money(0)),
+                Arguments.of(new DecemberDate(29), new Money(0)),
+                Arguments.of(new DecemberDate(30), new Money(0))
+        );
+    }
+
 }
